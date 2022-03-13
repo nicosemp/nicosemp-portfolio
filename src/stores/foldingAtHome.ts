86f465559e21d5df8ce312fcdbbd4834f0b59754
Nicolò Maria Semprini <nicosemp@gmail.com>
2022-03-14 00:24:25 +0100

    chore: add TODO

--- src/stores/foldingAtHome.ts
+++ src/stores/foldingAtHome.ts
@@ -1,6 +1,8 @@
 import { defineStore } from "pinia";
 import { useStorage } from "@vueuse/core";
 
+// TODO: read and print FAH score
+
 export const useFahStore = defineStore({
   id: "counter",
   state: () => ({
